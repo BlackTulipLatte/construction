21bd2c1fed12b98445268e39a9cfbfe2e741bc46
BlackTulipLatte <86974706+BlackTulipLatte@users.noreply.github.com>
2023-07-05 15:47:45 -0400

    added server for signin

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,12 @@
-const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: 'postgres',
-    password: 'postgres',
-    host: 'db-1.ctzk6yo38b2t.us-east-2.rds.amazonaws.com',
-    database: 'dbinit',
-    port: 5432,
-})
+const { Client } = require('pg');
 
-pool.connect();
+// Create a new instance of the client
+const client = new Client({
+  user: 'postgres',
+  password: 'postgres',
+  host: 'localhost',
+  database: 'vacationio',
+  port: 5432,
+});
 
-pool.query("INSERT INTO public.accounts VALUES (21002516, 't4zeng', 'test', 't4zeng@uwaterloo.ca')", (err, res) => {
-    if (!err) {
-      console.log(res.rows);
-    } else {
-      console.log(err);
-    }
-    pool.end();
-  });
-  
+module.exports = client;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,34 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors"); // Add this line
 const app = express();
-
+const client = require("./db");
 const port = 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+// Enable CORS
+app.use(cors()); // Add this line
+
+// Initialize the database connection
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+client.connect();
+
+// CRUD operations
+app.get('/users', async (req, res) => {
+    console.log(req.query);
+    const { email, password } = req.query;
+    console.log(email, password);
   
-app.listen(port, ()=>{console.log(`Server started on port ${port}`)})
+    try {
+      const query = 'SELECT COUNT(*) FROM accounts WHERE email = $1 AND password = $2';
+      const result = await client.query(query, [email, password]);
+  
+      const exists = result.rows[0].count > 0;
+      res.json({ exists });
+    } 
+    catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
