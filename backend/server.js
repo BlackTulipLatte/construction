01187b5dcd8e6772544d08d8b1f50b4e54c4052f
BlackTulipLatte <86974706+BlackTulipLatte@users.noreply.github.com>
2023-07-20 16:29:12 -0400

    added post for login and signup

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,12 @@ app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
+// Parse incoming request bodies with JSON payloads
+app.use(express.json());
+
+// Parse incoming request bodies with URL-encoded payloads
+app.use(express.urlencoded({ extended: true }));
+
 client.connect();
 
 // CRUD operations
@@ -32,3 +38,57 @@ app.get('/users', async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
+
+// POST to see if email exists
+app.post('/users', async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  
+  if (!password) {
+    return res.status(400).json({ error: 'Password is required' });
+  }
+
+  try {
+    const queryCheck = 'SELECT COUNT(*) FROM accounts WHERE email = $1';
+    const resultCheck = await client.query(queryCheck, [email]);
+    const exists = resultCheck.rows[0].count > 0;
+    console.log(exists);
+
+    if (exists) {
+      return res.json({ exists: true });
+    } else {
+      const queryInsert = 'INSERT INTO accounts (email, password) VALUES ($1, $2)';
+      await client.query(queryInsert, [email, password]);
+      return res.json({ exists: false, message: 'User added to the database.' });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// POST to login to the platform
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  
+  if (!password) {
+    return res.status(400).json({ error: 'Password is required' });
+  }
+
+  try {
+    const queryCheck = 'SELECT COUNT(*) FROM accounts WHERE email = $1 AND password = $2';
+    const resultCheck = await client.query(queryCheck, [email,password]);
+    const exists = resultCheck.rows[0].count > 0;
+    console.log(exists);
+
+    if (exists) {
+      return res.json({ exists: true, message: 'User exists' });
+    } else {
+      return res.json({ exists: false, message: 'User does not exist.' });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
